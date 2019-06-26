36073425ead23c20cb344967a03b5121cbac0954
Oscar Rodriguez <oscar@verify.as>
2019-06-26 17:45:28 +0200

    change response to 2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) =>{
-  res.send('Test')
+  res.send(2)
 })
 
 app.post('/', (req,res) => {
