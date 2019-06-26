82d35945e6fbeaee872746c3398c032edc0f1d9a
Oscar Rodriguez <oscar@verify.as>
2019-06-26 17:33:07 +0200

    initial config

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) =>{
+  res.send('Wolfgang')
+})
+
+app.post('/', (req,res) => {
+  res.send(req.body.username)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
