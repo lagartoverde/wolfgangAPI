5c22ccab0a6a73f3530d9483e358136c6aa28b74
Oscar Rodriguez <oscar@verify.as>
2019-06-29 21:43:41 +0200

    fixed heroku deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const fetch = require("node-fetch");
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const CWLSignup = require('./Models/CWLSignup')
 
